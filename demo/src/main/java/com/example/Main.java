8655fa4baef6416e0f280fdae36ebdc6249a43ff
Vinuitik <142442543+Vinuitik@users.noreply.github.com>
2024-09-15 11:34:03 +0300

    Update Main.java

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -5,7 +5,7 @@ import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new HopfieldUI(20, 20)); // WOW
+        SwingUtilities.invokeLater(() -> new HopfieldUI(20, 20)); // WOW ANYA IS SUPER AMAZING
         //System.out.println("null");
     }
 }
