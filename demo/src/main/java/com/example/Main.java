2d977dd9582e9e86de2cc182a7dcb04633fde8d6
Vinuitik <142442543+Vinuitik@users.noreply.github.com>
2024-09-21 09:51:43 +0300

    Update Main.java

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,11 +1,11 @@
 package com.example;
 //importing utility
 import javax.swing.SwingUtilities;
-//import com.example.HopfieldUI;
+import com.example.HopfieldUI;
 
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new HopfieldUI(20, 20)); // WOW ANYA IS SUPER AMAZING
-        //System.out.println("null");
+        /////System.out.println("null");
     }
 }
