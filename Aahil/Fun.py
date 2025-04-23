d58821de1dd12ae2e3764836bc82a5872b911b51
Vinuitik <142442543+Vinuitik@users.noreply.github.com>
2025-04-23 22:31:35 +0100

    Update Fun.py

--- Aahil/Fun.py
+++ Aahil/Fun.py
@@ -3,7 +3,7 @@ import mediapipe as mp
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
-
+    
 # MediaPipe setup 
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.5)
